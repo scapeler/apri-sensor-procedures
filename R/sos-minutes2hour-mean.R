48c2f0aa03b6e492b2687761a4e53192db5ab900
awiel <awiel@scapeler.com>
2018-10-24 11:37:05 +0200

    first version

--- R/sos-minutes2hour-mean.R
+++ R/sos-minutes2hour-mean.R
@@ -1,56 +1,71 @@
-#install.packages("ncdf4")
-library(ncdf4)
 #install.packages("RCurl")
 library(RCurl)
+library(plyr)
 
-ncname<-"knmi_tmp_10min_actual_data"
-ncfname <- paste('/opt/SCAPE604/knmi_files/',ncname, ".nc", sep = "")
+foiId<-"SCWM68C63A809492"
+foiInPmsa003<-paste("apri-sensor-pmsa003_",foiId,sep='')
+foiInBme280<-paste("apri-sensor-bme280_",foiId,sep='')
 
-# open a NetCDF file
-ncin <- nc_open(ncfname)
+endDate<-as.POSIXct(format(Sys.time(), "%Y%m%d%H"), format="%Y%m%d%H")
+persistDate<-endDate  # persist on hour as result from previous 60 minutes
+startDate<-endDate-3600
+endDate<-endDate-1  # HH:59:59
+startDateStr<-format(startDate,"%Y-%m-%dT%H:%M:%S%z")
+endDateStr<-format(endDate,"%Y-%m-%dT%H:%M:%S%z")
+persistDateStr<-format(persistDate,"%Y-%m-%dT%H:%M:%S%z")
 
-dd <- ncvar_get(ncin, "dd")
-ff <- ncvar_get(ncin, "ff")
-D1H <- ncvar_get(ncin, "D1H")
-R1H <- ncvar_get(ncin, "R1H")
-ta <- ncvar_get(ncin, "ta")
-rh <- ncvar_get(ncin, "rh")
+urlSensor<-paste("https://openiod.org/SCAPE604/openiod?SERVICE=WPS&REQUEST=Execute&identifier=transform_observation&action=getobservation"
+,"&sensorsystem=apri-sensor-pmsa003"
+, "&date_start=",startDateStr,"&date_end=",endDateStr
+, "&foi=",foiInPmsa003
+, "&offering=offering_0439_initial&format=csv",sep="");
+pmsA003 <- read.csv(urlSensor, header = FALSE, sep = ";", quote = "\"")
+#sensorType<-pmsA003$V3
+#sensorvalue<-pmsA003$V6
 
-t <- ncvar_get(ncin, "time")
-tunits <- ncatt_get(ncin, "time", "units")
+means<-aggregate(data=pmsA003,V6~V3, FUN=mean)
 
-tustr <- strsplit(tunits$value, " ")
-tmp<-unlist(tustr)[3]
-tijd<-as.POSIXct(tmp, format="%Y-%m-%d")
-#tijd2<-tijd+t
-tijd2<-strftime((tijd+t-3600) , "%Y-%m-%dT%H:%M:%S")
+observation<-''
+for (i in c(1:nrow(means))) { 
+  observation<-paste(observation,means$V3[i],':',round(means$V6[i],2),sep='')
+  if(i<nrow(means)) observation<-paste(observation,',',sep='')
+}
 
-ncvar_get(ncin, "time")
-stns <-ncvar_get(ncin, "station")
-stns[2:5]
-stnsname<-ncvar_get(ncin, "stationname")
-stns[stnsname=='HOEK VAN HOLLAND AWS']
-stns[stns=='06330']
-ddValue<-dd[stns=='06330']  # windrichting in graden 10 minute gemiddelde
-ffValue<-ff[stns=='06330']  # windspeed op 10m 10 minuten gemiddelde
-D1HValue<-D1H[stns=='06330']  # duration rainfall last hour
-R1HValue<-R1H[stns=='06330']  # rainfall in mm last hour
-taValue<-ta[stns=='06330']  # Air Temperature 1 Min Average
-rhValue<-rh[stns=='06330']  # Relative Humidity 1 Min Average
+foiOut<-paste(foiInPmsa003,'H',sep='')
+urlOut<-'https://openiod.org/SCAPE604/openiod?SERVICE=WPS&REQUEST=Execute&identifier=transform_observation&action=insertom&sensorsystem=apri-sensor-pmsa003&offering=offering_0439_initial&commit=true';
+urlOut <- paste(urlOut,'&foi=',foiOut,'&observation=',observation,'&measurementTime=',persistDateStr,sep='');
+urlOut
 
-nc_close(ncin)
-
-foi<-paste('KNMI','06330',sep='')
-observation<-paste('apri-sensor-knmi-dd:',ddValue,sep='')
-observation<-paste(observation,',','apri-sensor-knmi-ff:',ffValue,sep='')
-observation<-paste(observation,',','apri-sensor-knmi-D1H:',D1HValue,sep='')
-observation<-paste(observation,',','apri-sensor-knmi-R1H:',R1HValue,sep='')
-observation<-paste(observation,',','apri-sensor-knmi-ta:',taValue,sep='')
-observation<-paste(observation,',','apri-sensor-knmi-rh:',rhValue,sep='')
-url1<-'https://openiod.org/SCAPE604/openiod?SERVICE=WPS&REQUEST=Execute&identifier=transform_observation&action=insertom&sensorsystem=apri-sensor-knmi10m&offering=offering_knmi10m_initial&commit=true';
-url <- paste(url1,'&foi=',foi,'&observation=',observation,'&measurementTime=',tijd2,sep='');
-url
-
-myfile <- getURL(url, ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
+myfile <- getURL(urlOut, ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
 myfile
-quit(save = "no",status = 0)
+
+# BME280
+urlSensor<-paste("https://openiod.org/SCAPE604/openiod?SERVICE=WPS&REQUEST=Execute&identifier=transform_observation&action=getobservation"
+                 ,"&sensorsystem=apri-sensor-bme280"
+                 , "&date_start=",startDateStr,"&date_end=",endDateStr
+                 , "&foi=",foiInBme280
+                 , "&offering=offering_0439_initial&format=csv",sep="");
+bme280 <- read.csv(urlSensor, header = FALSE, sep = ";", quote = "\"")
+#sensorType<-pmsA003$V3
+#sensorvalue<-pmsA003$V6
+
+means<-aggregate(data=bme280,V6~V3, FUN=mean)
+
+observation<-''
+for (i in c(1:nrow(means))) { 
+  observation<-paste(observation,means$V3[i],':',round(means$V6[i],2),sep='')
+  if(i<nrow(means)) observation<-paste(observation,',',sep='')
+}
+
+foiOut<-paste(foiInBme280,'H',sep='')
+urlOut<-'https://openiod.org/SCAPE604/openiod?SERVICE=WPS&REQUEST=Execute&identifier=transform_observation&action=insertom&sensorsystem=apri-sensor-bme280&offering=offering_0439_initial&commit=true';
+urlOut <- paste(urlOut,'&foi=',foiOut,'&observation=',observation,'&measurementTime=',persistDateStr,sep='');
+urlOut
+
+myfile <- getURL(urlOut, ssl.verifyhost=FALSE, ssl.verifypeer=FALSE)
+myfile
+
+
+
+#quit(save = "no",status = 0)
+
